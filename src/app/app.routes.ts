1092a6d8b1ed6b308a74f3832bd47be0ca46ac96
Pabu24 <pawel.budnik.1920@gmail.com>
2023-11-26 16:56:12 +0100

    Dodanie car i car-list jeszcze raz bo nie dodało

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CarComponent } from './car/car.component'; 
+
+export const routes: Routes = [
+    {path: 'car', component: CarComponent}
+];
---
+++ src/app/car-list/car-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-car-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './car-list.component.html',
+  styleUrl: './car-list.component.css'
+})
+export class CarListComponent {
+
+}
