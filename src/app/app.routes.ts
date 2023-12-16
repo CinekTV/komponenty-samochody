31babf75b31a6b6e4126ea74dfb4145eaeaf3086
Cinek <cinekv15@gmail.com>
2023-12-16 22:08:12 +0100

    Sortowanie czy coś

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterLink, RouterLinkActive, RouterOutlet, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.componenet'; 
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component'; 
@@ -8,10 +9,9 @@ import {RegisterComponent} from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { CarServiceService } from './car-service.service';
-// import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
-    // {path: '**', component: PageNotFoundComponentComponent},
     {path: '', redirectTo: '/contact', pathMatch: 'full'},
     {path:'app',component:AppComponent},
     {path:'login',component:LoginComponent},
@@ -21,7 +21,13 @@ export const routes: Routes = [
     {path: 'car-rental/:id', component: CarRentalComponent},
     {path: 'car-list', component: CarListComponent},
     {path: 'contact', component:ContactComponent},
-    {path: 'rental-list', component:RentalListComponent}
+    {path: 'rental-list', component:RentalListComponent},
+    {path: '**', component: PagenotfoundComponent, pathMatch: 'full'}
 ];
 
-
+@NgModule({ 
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule], 
+    providers: [] 
+}) 
+export class AppRoutingModule { } 
--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink,RouterLinkActive } from '@angular/router';
 import { Router } from '@angular/router';
@@ -9,25 +9,74 @@ import { Car } from '../../types/car';
 @Component({
   selector: 'app-car-list',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, FormsModule],
   providers: [CarServiceService],
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.css'
 })
-export class CarListComponent implements OnInit{
+export class CarListComponent implements OnInit {
+  title = 'samochody';
 
-    title = 'samochody';
-
-    cars: Car[] = [];
+  cars: Car[] = [];
 
 
-    constructor(private carService: CarServiceService, private router: Router) {}
-    
-
-    ngOnInit(): void {
-      this.cars=this.carService.getCars();
-    }
+  constructor(private carService: CarServiceService, private router: Router) { }
   
+
+  sortOptions = [
+    { value: 'id', label: 'ID' },
+    { value: 'brand', label: 'Marka' },
+    { value: 'model', label: 'Model' },
+    { value: 'regPlate', label: 'Numer rejestracyjny' },
+    { value: 'status', label: 'Status' },
+    { value: 'productionYear', label: 'Rok produkcji' }
+  ];
+
+
+  selectedSortOption: string | undefined;
+  // currentSortProperty: string | null = null;
+  sortBy() {
+    if (this.selectedSortOption) {
+      switch (this.selectedSortOption) {
+        case 'id':
+          this.cars.sort((a, b) => a.id - b.id);
+          break;
+        case 'brand':
+          this.cars.sort((a, b) => a.brand.localeCompare(b.brand));
+          break;
+        case 'model':
+          this.cars.sort((a, b) => a.model.localeCompare(b.model));
+          break;
+        case 'regPlate':
+          this.cars.sort((a, b) => a.regPlate.localeCompare(b.regPlate));
+          break;
+        case 'status':
+          this.cars.sort((a, b) => a.status - b.status);
+          break;
+        case 'productionYear':
+          this.cars.sort((a, b) => a.productionYear - b.productionYear);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  
+  // Function to handle dynamic sorting
+  // sortBy(property: string) {
+  //   if (this.currentSortProperty === property) {
+  //     // If already sorted by the same property, reverse the order
+  //     this.cars.reverse();
+  //   } else {
+  //     this.currentSortProperty = property;
+  //     this.cars.sort((a, b) => (a > b) ? 1 : -1);
+  //   }
+  // }
+
+  ngOnInit(): void {
+    this.cars = this.carService.getCars();
+  }
+
   navigateToRent(carId: number) {
     this.router.navigate(['/car-rental', carId]);
   }
